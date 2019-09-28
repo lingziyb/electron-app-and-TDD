d07efc132273f236bbafb51b90969b25cddbc567
zhaolingli <zhaolinglizll@qq.com>
2019-09-28 17:11:46 +0800

    初始化项目  npm run dev/build运行起来

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # electron-app-and-TDD
-将Electron和Vue相结合来构建跨平台的桌面应用，结合单元测试。
+
+将 Electron 和 Vue 相结合来构建跨平台的桌面应用，结合单元测试。
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn run build
+```
+
+### Run your tests
+
+```
+yarn run test
+```
+
+### Lints and fixes files
+
+```
+yarn run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ app.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  if (process.env.NODE_DNV === "DEV") {
+    win.loadURL("http://localhost:8080/");
+  } else {
+    win.loadURL(`file://${process.cwd()}/dist/index.html`);
+  }
+
+  win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win == null) {
+    createWindow();
+  }
+});
